b3bd1f991814706e1d1fef1ee6b33231435ff869
Andrew Carlson <rotexdev@gmail.com>
2016-11-17 18:03:39 -0800

    Use key=val serialization format

--- app/instance-initializers/fastboot/ember-data-fastboot.js
+++ app/instance-initializers/fastboot/ember-data-fastboot.js
@@ -4,12 +4,20 @@ export function initialize(applicationInstance) {
 
   shoebox.put('ember-data-store', {
     get records() {
-      return Object.keys(store.typeMaps).map(k => {
-        let name = store.typeMaps[k].type.modelName;
-        return store.peekAll(name).toArray();
-      }).reduce((a,b) => a.concat(b), [])
-        .map(record => record.serialize({ includeId: true}))
-        .reduce((a,b) => { a.data.push(b.data); return a; }, { data: [] });
+      return Object.keys(store.typeMaps)
+        .map(key => {
+          let modelName = store.typeMaps[key].type.modelName;
+          return {
+            modelName: modelName,
+            data: store.peekAll(modelName)
+                      .toArray()
+                      .map(record => record.serialize({ includeId: true }) )
+          };
+        })
+        .reduce((a, current) => {
+          a[current.modelName] = current.data;
+          return a;
+        }, {});
     }
   });
 }
