ffa405814c66677bee965ffd2a0a955ad58ead36
Andrew Carlson <rotexdev@gmail.com>
2017-10-31 11:25:33 -0700

    Support Fastboot 1.0

--- app/instance-initializers/ember-data-fastboot.js
+++ app/instance-initializers/ember-data-fastboot.js
@@ -21,5 +21,9 @@ export function initialize(applicationInstance) {
 
 export default {
   name: 'ember-data-fastboot',
-  initialize
+  initialize() {
+    if (typeof FastBoot === 'undefined') {
+      initialize(...arguments);
+    }
+  }
 };
